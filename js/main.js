1027e84f04662e99255496d6df0de26e8f9e8ca4
Mickael <mickael.spilmont.fr@gmail.com>
2019-01-21 17:40:34 +0100

    Affichage des liens grace à une boucle

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-const nom = 'Regina';
-let html = 'images/' + nom.toLowerCase() + '.jpg';
-html = `<a href="${html}">${html}</a>`;
+// const nom = 'Regina';
+// let html = 'images/' + nom.toLowerCase() + '.jpg';
+// html = `<a href="${html}">
+//   <img src="${html}"/>
+//   <h4>Regina</h4>
+// </a>`;
+//
+// console.log(html);
+//
+// document.querySelector(".pizzasContainer").innerHTML = html;
 
-console.log(html);
+const noms = ['Regina', 'Napolitaine', 'Spicy'];
+const liens = [];
+let html = '';
 
+for (let i = 0 ; i < noms.length ; i++) {
+  html += `<a href="images/${noms[i].toLowerCase()}.jpg"
+              <img src="images/${noms[i].toLowerCase()}.jpg"/>
+              <h4>${noms[i]}</h4>
+            </a>`;
+}
 document.querySelector(".pizzasContainer").innerHTML = html;
