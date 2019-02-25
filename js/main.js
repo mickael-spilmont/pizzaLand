e4a749e1c42071586ebb19f6f2004be3916d881c
Mickael <mickael.spilmont.fr@gmail.com>
2019-02-25 17:36:39 +0100

    Préparation terminé Bran is flying

--- js/main.js
+++ js/main.js
@@ -1,53 +1,4 @@
-// D. Manipulation des chaînes
-
-// D.2. Créer une constante appelée `nom` et y assigner la chaîne de caractères `Regina`
-const nom = 'Regina';
-// D.3. Créer une constante nommée `url`.
-const url = 'images/'+nom.toLowerCase()+'.jpg';
-// D.4. Créer la variable `html`
-let html = `<a href="${url}">${url}</a>`;
-console.log( html );
-// D.5. C'est le moment d'afficher le contenu de la variable `html` dans la page.
-document.querySelector(".pizzasContainer").innerHTML = html;
-// D.6. Modifier encore la variable html avant son affichage
-html = `<a href="${url}">
-	<img src="${url}" />
-	<h4>${nom}</h4>
-</a>`;
-document.querySelector(".pizzasContainer").innerHTML = html;
-
-//F. Manipulation des tableaux
-let data = [ 'Regina', 'Napolitaine', 'Spicy' ];
-html = '';
-// F.1. boucle for :
-for (let i = 0; i < data.length; i++) {
-	const nom = data[i],
-	url = `images/${nom.toLowerCase()}.jpg`;
-	html += `<a href="${url}">
-		<img src="${url}" />
-		<h4>${nom}</h4>
-	</a>`;
-}
-document.querySelector(".pizzasContainer").innerHTML = html;
-// F.2. Array.forEach :
-html = '';
-data.forEach( nom => {
-	const url = `images/${nom.toLowerCase()}.jpg`;
-	html += `<a href="${url}">
-		<img src="${url}" />
-		<h4>${nom}</h4>
-	</a>`;
-})
-document.querySelector(".pizzasContainer").innerHTML = html;
-// F.3. Array.map + Array.join :
-html = data.map( nom => `<a href="images/${nom.toLowerCase()}.jpg">
-	<img src="images/${nom.toLowerCase()}.jpg" />
-	<h4>${nom}</h4>
-</a>`).join('');
-document.querySelector(".pizzasContainer").innerHTML = html;
-
-// G. Les objets littéraux
-data = [
+const data = [
 	{
 		nom: 'Regina',
 		base: 'tomate',
@@ -68,20 +19,15 @@ data = [
 	},
 ];
 
-html = '';
-data.forEach( pizza => {
-	const url = `images/${pizza.nom.toLowerCase()}.jpg`;
-	html += `<article class="media">
-		<a href="${url}">
-			<img src="${url}" />
-			<section class="infos">
-				<h4>${pizza.nom}</h4>
-				<ul>
-					<li>Prix petit format : ${pizza.prix_petite.toFixed(2)} €</li>
-					<li>Prix grand format : ${pizza.prix_grande.toFixed(2)} €</li>
-				</ul>
-			</section>
-		</a>
-	</article>`;
-})
-document.querySelector(".pizzasContainer").innerHTML = html;
+class Animal {
+	name;
+	constructor(name) {
+		this.name = name
+	}
+	fly() {
+		console.log(`${this.name} is flying !`);
+	}
+}
+
+const threeEyedRaven = new Animal('Bran');
+threeEyedRaven.fly();
