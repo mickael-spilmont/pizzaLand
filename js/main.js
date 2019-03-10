fcc2dec81e08265d5a595e95556ecddd78113721
Mickael Spilmont <mickael.spilmont.fr@gmail.com>
2019-03-10 20:11:21 +0100

    recursivité non fonctionelle

--- js/main.js
+++ js/main.js
@@ -29,5 +29,56 @@ class Animal {
 	}
 }
 
-const threeEyedRaven = new Animal('Bran');
-threeEyedRaven.fly();
+class Component {
+	tag;
+	attribute;
+	children;
+	constructor(tag, attribute, children) {
+		this.tag = tag;
+		this.attribute = attribute;
+		this.children = children;
+	}
+	render() {
+		let str;
+		if (this.children) {
+			str = `<${this.tag}>
+						${this.renderChildren()}
+					</${this.tag}>`;
+		} else {
+			str = `<${this.tag} ${this.renderAttribute()}/>`
+		}
+		return str;
+	}
+	renderAttribute() {
+		if (this.attribute !== null) {
+			return `${this.attribute.name}=\"${this.attribute.value}\"`;
+		}
+		return null;
+	}
+	renderChildren() {
+		let str = "";
+		if (this.children instanceof Array) {
+			this.children.forEach(element => {
+				if (element instanceof Component) {
+					return element.render();
+				}
+				str += element;
+			});
+		} else {
+			str = this.children;
+		}
+		return str;
+	}
+}
+
+// const threeEyedRaven = new Animal('Bran');
+// threeEyedRaven.fly();
+
+// const title = new Component('h1', null, ['La', ' ', 'carte']);
+// document.querySelector('.pageTitle').innerHTML = title.render();
+
+// const img = new Component('img', {name: 'src', value: 'images/regina.jpg'});
+// document.querySelector('.pizzasContainer').innerHTML = img.render();
+
+const c = new Component( 'article', {name:'class', value:'media'}, [new Component('img', null, 'images/regina.jpg'), 'Regina']);
+document.querySelector( '.pizzasContainer' ).innerHTML = c.render();
